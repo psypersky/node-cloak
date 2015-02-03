34a9fc77b62ae9235cf1f8960769779580caf9b3
David Oliveros <dato.oliveros@gmail.com>
2015-02-03 01:24:00 -0600

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 var fs = require('fs');
 var env = process.env.NODE_ENV || 'default';
-var envConfigPath = fs.existsSync('./'+env) ? './'+env :'default';
+var envConfigPath = fs.existsSync('./'+env) ? './'+env :'./default';
 
 // environment-based config file
-var config = require('./'+env);
+var config = require(envConfigPath);
 
 config.PROXY_COUNT = config.tor ? config.tor.instances : 4;
 config.PORT_RANGE_START = config.tor ? config.tor.port : 10770;
